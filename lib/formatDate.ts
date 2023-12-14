9a90ffe84435fb9eabc59ebc91c2ad2e6d01bb74
Ronilson Alves <falecom@ronilsonalves.com>
2023-12-14 14:29:09 -0300

    🚧 chore: Add a formatDate util
    – Added a util function to format datetime.

--- lib/formatDate.ts
+++ lib/formatDate.ts
@@ -1,6 +1,5 @@
 export function formatDate(date: Date, locale?: string): string {
-  // TODO: use locale to format date based on user's locale.
-  return new Date(date).toLocaleDateString("en-US", {
+  return new Date(date).toLocaleDateString(locale, {
     day: "numeric",
     month: "long",
     year: "numeric",
