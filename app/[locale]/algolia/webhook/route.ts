9cd3051a56ff422f6ede8088325593a3c9e63ca5
Ronilson Alves <falecom@ronilsonalves.com>
2023-12-27 16:01:59 -0300

    🚧 feat: Init Algolia search implemetation
    — Add sanity-algolia plugin to allow the creation of a search component powered by algolia engine.
    — Created a Sanity webhook to allow the shared index between Sanity and Algolia

---
+++ app/[locale]/algolia/webhook/route.ts
@@ -0,0 +1,147 @@
+import { NextRequest, NextResponse } from "next/server";
+import algoliasearch from "algoliasearch";
+import { createClient, type SanityDocumentStub } from "@sanity/client";
+import indexer from "sanity-algolia";
+import { WebhookBody } from "sanity-algolia/dist/types";
+
+const config = {
+  algoliaAdminApiKey: process.env.ALGOLIA_ADMIN_API_KEY,
+  algoliaApplicationId: process.env.ALGOLIA_APPLICATION_ID,
+  algoliaPtIndexName: process.env.ALGOLIA_PT_INDEX_NAME,
+  algoliaIndexName: process.env.ALGOLIA_INDEX_NAME,
+  sanityProjectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  sanityDataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  sanityReadToken: process.env.SANITY_API_TOKEN,
+  sanityWebhookSecret: process.env.SANITY_WEBHOOK_SECRET,
+};
+
+const algolia = algoliasearch(
+  config.algoliaApplicationId!,
+  config.algoliaAdminApiKey!
+);
+
+const sanity = createClient({
+  projectId: config.sanityProjectId!,
+  dataset: config.sanityDataset!,
+  // If your dataset is private you need to add a read token.
+  // You can mint one at https://manage.sanity.io,
+  token: config.sanityReadToken!,
+  apiVersion: "2021-03-25",
+  useCdn: false,
+});
+
+/**
+ *  This function receives webhook POSTs from Sanity and updates, creates or
+ *  deletes records in the corresponding Algolia indices.
+ */
+export async function POST(req: NextRequest, res: NextResponse) {
+  // Tip: Add webhook secrets to verify that the request is coming from Sanity.
+  // See more at: https://www.sanity.io/docs/webhooks#bfa1758643b3
+  if (req.headers.get("content-type") !== "application/json") {
+    return new Response(
+      JSON.stringify({
+        message: "Bad request",
+      }),
+      { status: 400 }
+    );
+  }
+
+  // Configure this to match an existing Algolia index name
+  const algoliaIndex = algolia.initIndex(config.algoliaIndexName!);
+
+  const sanityAlgolia = indexer(
+    // The first parameter maps a Sanity document type to its respective Algolia
+    // search index. In this example both `post` and `article` Sanity types live
+    // in the same Algolia index. Optionally you can also customize how the
+    // document is fetched from Sanity by specifying a GROQ projection.
+    //
+    // In this example we fetch the plain text from Portable Text rich text
+    // content via the pt::text function.
+    //
+    // _id and other system fields are handled automatically.
+    {
+      post: {
+        index: algoliaIndex,
+        projection: `{
+          title,
+          "path": slug.current,
+          "body": pt::text(body)
+        }`,
+      },
+      // For the article document in this example we want to resolve a list of
+      // references to authors and get their names as an array. We can do this
+      // directly in the GROQ query in the custom projection.
+      article: {
+        index: algoliaIndex,
+        projection: `{
+          heading,
+          "body": pt::text(body),
+          "authorNames": authors[]->name
+        }`,
+      },
+    },
+
+    // The second parameter is a function that maps from a fetched Sanity document
+    // to an Algolia Record. Here you can do further mutations to the data before
+    // it is sent to Algolia.
+    (document: SanityDocumentStub) => {
+      switch (document._type) {
+        case "post":
+          return Object.assign({}, document, {
+            custom: "An additional custom field for posts, perhaps?",
+          });
+        case "article":
+          return {
+            title: document.heading,
+            body: document.body,
+            authorNames: document.authorNames,
+          };
+        default:
+          return document;
+      }
+    },
+    // Visibility function (optional).
+    //
+    // The third parameter is an optional visibility function. Returning `true`
+    // for a given document here specifies that it should be indexed for search
+    // in Algolia. This is handy if for instance a field value on the document
+    // decides if it should be indexed or not. This would also be the place to
+    // implement any `publishedAt` datetime visibility rules or other custom
+    // visibility scheme you may be using.
+    (document: SanityDocumentStub) => {
+      if (document.hasOwnProperty("isHidden")) {
+        return !document.isHidden;
+      }
+      return true;
+    }
+  );
+
+  console.log(await req.body?.getReader().read())
+
+  const parsedWebhookBody = await req.body
+    ?.getReader()
+    .read()
+    .then(({ value }) => {
+      console.log(value)
+      return JSON.parse(new TextDecoder().decode(value));
+    });
+
+  // Finally connect the Sanity webhook payload to Algolia indices via the
+  // configured serializers and optional visibility function. `webhookSync` will
+  // inspect the webhook payload, make queries back to Sanity with the `sanity`
+  // client and make sure the algolia indices are synced to match.
+  return sanityAlgolia
+    .webhookSync(sanity, parsedWebhookBody as WebhookBody)
+    .then(() => {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Success" }),
+      };
+    })
+    .catch((err) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ message: "Something went wrong" }),
+      };
+    });
+}
