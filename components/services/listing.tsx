e1bebc58a29852c68fde20c80c504314fd01d0dd
Ronilson Alves <falecom@ronilsonalves.com>
2024-02-13 19:58:02 -0300

    🩹 fix: services page responsive layout
    – Updated the Tailwind class in services page to support a better responsive layout in all screens.
    – Also, changed the location of some components related to the services page.

--- components/services/listing.tsx
+++ components/services/listing.tsx
@@ -8,9 +8,9 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-import ServiceCarouselItem from "./carousel";
-const CalWidget = lazy(() => import("../institutional/services/cal-widget"));
-import { ContactForm } from "../institutional/about/form";
+import ServiceCarouselItem from "@/components/services/carousel";
+const CalWidget = lazy(() => import("@/components/services/cal-widget"));
+import { ContactForm } from "@/components/services/form";
 
 interface ServiceListingProps {
   services: SanityDocument[];
@@ -47,18 +47,20 @@ export default function ServiceListing({
         </CardContent>
       </Card>
       {/*Contact & meeting schedule card */}
-      <div className="">
+      <div className="my-8 flex flex-col justify-center gap-4 px-4 md:mx-8 md:px-16">
         <ContactForm />
       </div>
-      <div className="flex flex-row place-items-center text-center">
-        <div className="w-1/3 border-2" />
+      <div className="mx-auto flex flex-row place-items-center justify-center gap-2 text-center">
+        <div className="min-w-44 border-2" />
         <span className="w-1/3 text-sm font-semibold">{t("OrText")}</span>
-        <div className="w-1/3 border-2" />
+        <div className="min-w-44 border-2" />
       </div>
-      <div className="w-full">
-        <h2 className="mb-4 text-3xl font-semibold">{t("Schedule")}</h2>
-        <p className="text-md mb-4 md:text-xl">{t("Description")}</p>
-        <div className="">
+      <div className="mt-8 flex flex-col justify-center gap-4 px-4 md:mx-8 md:px-16">
+        <div className="mx-auto w-full md:max-w-3xl xl:max-w-6xl">
+          <h2 className="mb-4 text-3xl font-semibold ">{t("Schedule")}</h2>
+          <p className="text-md mb-4 md:text-xl">{t("Description")}</p>
+        </div>
+        <div className="mx-auto mb-8 w-full md:max-w-3xl xl:max-w-6xl">
           <CalWidget />
         </div>
       </div>
